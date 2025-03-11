6d22fa70a0591cdfd2a4b6ea05f5486f0701d712
Beslin <Wilfried.Beslin@dfo-mpo.gc.ca>
2025-03-11 16:53:40 -0300

    Overhaul the spectrogram parameter parsing system in dsp.Spectrogram to add flexibility and robustness (plus a few more minor changes)

--- +MUCA/+dsp/Spectrogram.m
+++ +MUCA/+dsp/Spectrogram.m
@@ -4,15 +4,19 @@ classdef Spectrogram
 %
 % This class tries to keep things simple and does not store spectrogram 
 % parameters; it only stores the time vector, frequency vector, PSD matrix,
-% and other variables related to or derived from these.
+% and other variables related to or derived from these. That said, the
+% class does define a library of preset (sampling frequency-dependent)
+% spectrogram parameters that is accessible from the static method
+% "get_preset_parameters". Spectrogram objects may be constructed using one
+% of the presets, or with a custom set of parameters.
 %
 % As a convenience feature, this class allows users to edit the PSD matrix
 % directly (using both linear and dB scale). This can be useful, e.g., to
-% translate the whole matrix such that the maximum equals 0 dB, or to set a
-% group of values to zero, etc.
+% translate the whole matrix such that the maximum equals 0 dB, or to
+% normalize frequency bins, or to set a group of values to zero, etc.
 %
 % Written by Wilfried Beslin
-% Last Updated 2025/03/10
+% Last Updated 2025/03/11
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
@@ -45,7 +49,7 @@ classdef Spectrogram
     end
     
     
-    % Constructor
+    % Constructor ---------------------------------------------------------
     methods
         function obj = Spectrogram(x, fs, varargin)
         % Creates a Spectrogram object.
@@ -61,13 +65,31 @@ classdef Spectrogram
         %   Optional (Name-Value Pairs)
         %   ...............................................................
         %   "params" - may be either a string specifying preset spectrogram
-        %       parameters to use, or a struct with fields 'wintype', 
-        %       'winsize', 'ovl', and 'nfft'. If using a string, options 
-        %       are:
-        %           'meridian' (default)
-        %           'vanderlaan'
-        %           'flogeras'
-        %           'baumgartner'.
+        %       parameters to use, or a struct with the following fields:
+        %           'winfcn' - may be either a char string defining a
+        %               window function to use (such as 'hamming'), or a
+        %               numeric vector that defines a custom window
+        %               function (normally with values between 0 and 1).
+        %           'winsize' (CONDITIONAL) - an integer that defines the
+        %               size of the window (a.k.a. frame), in samples. If
+        %               'winfcn' is a vector, this parameter is ignored.
+        %           'winparams' (OPTIONAL) - a cell array of arguments for
+        %               certain window functions that can accept additional
+        %               inputs other than size (for example, the 'blackman'
+        %               window accepts a flag for either symmetric or
+        %               periodic sampling). This field is completely
+        %               optional, and is ignored if 'winfcn' is a vector.
+        %           'novl' - The number of samples to overlap between
+        %               successive STFT frames. This must be less than
+        %               'winsize' (or the length of 'winfcn' if 'winfcn' is
+        %               a vector).
+        %           'nfft' - The number of points to use in FFT. It must be
+        %               greater than or equal to 'winsize' (or the length
+        %               of 'winfcn' if 'winfcn' is a vector).
+        %       For string inputs, the valid options are defined in the
+        %       static method "get_preset_parameters" at the end of this
+        %       class file.
+        %       The default is 'meridian'.
         %   ...............................................................
         %   "smooth" - Boolean value specifying whether or not the
         %       spectrogram should be smoothed with a Gaussian kernal.
@@ -88,58 +110,33 @@ classdef Spectrogram
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             
             p = inputParser;
+            p.addRequired('x', @(v)validateattributes(v,{'numeric'},{'vector'}))
+            p.addRequired('fs', @(v)validateattributes(v,{'numeric'},{'positive','integer','scalar'}))
             p.addParameter('params', 'meridian');
             p.addParameter('smooth', false);
             p.addParameter('t_start', 0);
 
-            p.parse(varargin{:});
-            params = p.Results.params;
+            p.parse(x, fs, varargin{:});
+            params_input = p.Results.params;
             do_smooth = p.Results.smooth;
             t0 = p.Results.t_start;
 
-            % parameters
-            if ischar(params)
-                switch params
-                    case 'meridian'
-                        specparam_winfcn = @hamming; % not actually sure what it is
-                        specparam_winsize = round(fs*0.256);
-                        specparam_ovl = specparam_winsize - round(fs*0.032);
-                        specparam_nfft = 2^nextpow2(fs*(2048/8000));
-                    case 'vanderlaan'
-                        specparam_winfcn = @hamming; % not actually sure what it is
-                        specparam_winsize = round(fs*0.2);
-                        specparam_ovl = specparam_winsize - round(fs*0.064);
-                        specparam_nfft = 2^nextpow2(fs/5);
-                    case 'flogeras'
-                        specparam_winfcn = @hamming; % not actually sure what it is
-                        specparam_winsize = 256; % scipy default
-                        specparam_ovl = round(0.5*specparam_winsize);
-                        specparam_nfft = 1024;
-                    case 'baumgartner'
-                        specparam_winfcn = @hann;
-                        specparam_winsize = round(fs*(640/2048));
-                        specparam_ovl = round(0.8*specparam_winsize);
-                        specparam_nfft = specparam_winsize;
-                    otherwise
-                        error('Bad spectrum type')
-                end
+            % process spectrogram parameters
+            if isstruct(params_input)
+                params_struct = params_input;
             else
-                % read parameters from struct
-                specparam_winfcn = params.wintype;
-                specparam_winsize = params.winsize;
-                specparam_ovl = params.ovl;
-                specparam_nfft = params.nfft;
+                params_struct = obj.get_preset_parameters(params_input, fs);
             end
-            specparam_winvec = feval(specparam_winfcn, specparam_winsize);
+            [winvec, novl, nfft] = obj.parse_parameters(params_struct);
 
             % compute spectrogram
             %%% using the default 'spectrumType' argument, which returns
             %%% the PSD
-            [~, spec_f, spec_t, spec_psd] = spectrogram(x, specparam_winvec, specparam_ovl, specparam_nfft, fs);
+            [~, spec_f, spec_t, spec_psd] = spectrogram(x, winvec, novl, nfft, fs);
             
             % determine time and frequency step
-            delta_t = (specparam_winsize - specparam_ovl)/fs;
-            delta_f = fs/specparam_nfft;
+            delta_t = (numel(winvec) - novl)/fs;
+            delta_f = fs/nfft;
             
             % set object properties
             obj.psd_priv = spec_psd;
@@ -157,11 +154,8 @@ classdef Spectrogram
     end
     
     
-    %% SET/GET METHODS ====================================================
+    %% "SET" METHODS ======================================================
     methods
-        
-        % set -------------------------------------------------------------
-        
         % set.psd .........................................................
         function obj = set.psd(obj, val)
             assert(all(size(val) == [obj.nf, obj.nt]))
@@ -179,10 +173,11 @@ classdef Spectrogram
             validateattributes(val, {'numeric','duration','datetime'}, {'scalar'})
             obj.t_start = val;
         end
-        
-        
-        % get -------------------------------------------------------------
-        
+    end
+
+
+    %% "GET" METHODS ======================================================
+    methods
         % get.psd .........................................................
         function val = get.psd(obj)
             val = obj.psd_priv;
@@ -213,13 +208,11 @@ classdef Spectrogram
         function val = get.nt(obj)
             val = numel(obj.t_relative);
         end
-        
     end
     
     
-    %% METHODS - PUBLIC ===================================================
+    %% PUBLIC METHODS =====================================================
     methods (Access = public)
-        
         % smooth ----------------------------------------------------------
         function obj = smooth(obj)
         % Smooth spectrogram with a 2D Gaussian kernel function.
@@ -250,7 +243,6 @@ classdef Spectrogram
             % set smoothed matrix as the PSD of the new object
             obj.psd = psd_smooth;
         end
-        
         
         % truncate --------------------------------------------------------
         function obj = truncate(obj, varargin)
@@ -338,7 +330,6 @@ classdef Spectrogram
             end
         end
         
-        
         % plot ------------------------------------------------------------
         function varargout = plot(obj, varargin)
         % Produce a plot of the spectrogram.
@@ -403,8 +394,6 @@ classdef Spectrogram
             
             % set axes limits
             axis(ax, 'tight');
-            %drawnow % required, otherwise weird things happen when calling 'axis' in succesion multiple times
-            %axis(ax, 'auto x');
             
             % return plot if needed
             if nargout > 0
@@ -412,7 +401,6 @@ classdef Spectrogram
             end
         end
         
-  
         % surf ------------------------------------------------------------
         function varargout = surf(obj, varargin)
         % create a Surface object from spectrogram data
@@ -535,6 +523,192 @@ classdef Spectrogram
                 varargout = {s};
             end
         end
-        
+    end
+
+
+    %% STATIC METHODS (Public) ============================================
+    methods (Access = public, Static)
+        % parse_parameters ------------------------------------------------
+        function [winvec, novl, nfft] = parse_parameters(params_struct)
+        % Takes a parameter struct as input and returns variables to be
+        % used as input for the "spectrogram" function. The rules of the
+        % parameter struct are explained in the header of the class
+        % constructor.
+        % This method is really intended for internal use only.
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+            % make sure the fields are all expected
+            valid_param_fields = {...
+                'winfcn',...
+                'winsize',...
+                'winparams',...
+                'novl',...
+                'nfft'}; 
+            param_fields = fieldnames(params_struct);
+            assert(all(ismember(param_fields, valid_param_fields)), 'The parameter struct contains invalid fields')
+
+            % check if 'winfcn' is a string or a vector, and process
+            % accordingly
+            if ischar(params_struct.winfcn) || isstring(params_struct.winfcn)
+                % make sure 'winsize' is specified
+                assert(ismember('winsize',param_fields), 'The field ''winsize'' must be specified when ''winfcn'' is a string');
+
+                % process window string input
+                winfcn_name = lower(char(params_struct.winfcn));
+
+                %%% convert name to a MATLAB function handle
+                winfcn_fcn = str2func(winfcn_name);
+
+                %%% extract any optional window arguments
+                if ismember('winparams', param_fields)
+                    assert(iscell(params_struct.winparams), 'Expected ''winparams'' to be a cell array')
+                    winfcn_args = params_struct.winparams;
+                else
+                    winfcn_args = {};
+                end
+
+                %%% create the window vector
+                winvec = window(winfcn_fcn, params_struct.winsize, winfcn_args{:});
+            else
+                % process window vector input
+                %%% make sure the window is a numeric vector
+                validateattributes(params_struct.winfcn, {'numeric'},{'vector'});
+
+                %%% if 'winsize' or 'winparams' were specified, issue a
+                %%% warning that they will be ignored
+                if any(ismember({'winsize','winparams'}, param_fields))
+                    warning('''winsize'' and/or ''winparams'' exist, but ''winvec'' was specified directly as a vector and does not require these parameters. They will be ignored.')
+                end
+                
+                %%% assign the window vector
+                winvec = params_struct.winfcn;
+            end
+
+            % validate 'novl'
+            validateattributes(params_struct.novl, {'numeric'}, {'integer','scalar','nonnegative','<',numel(winvec)}, '', 'novl');
+            novl = params_struct.novl;
+
+            % validate 'nfft'
+            validateattributes(params_struct.nfft, {'numeric'}, {'integer','scalar','positive','>=',numel(winvec)}, '', 'nfft');
+            nfft = params_struct.nfft;
+        end
+
+        % get_preset_parameters -------------------------------------------
+        function params = get_preset_parameters(preset_name, fs)
+        % Returns a struct with pre-determined spectrogram parameters.
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+            % parse input
+            p = inputParser();
+            p.addRequired('preset_name', @(v)validateattributes(v,{'char','string'},{}))
+            p.addRequired('fs', @(v)validateattributes(v,{'numeric'},{'scalar','positive','integer'}))
+            p.parse(preset_name, fs)
+
+            preset_name = lower(char(preset_name));
+            valid_presets = {...
+                'meridian',...
+                'bio_narw_analysis',...
+                'bio_muppet',...
+                'vanderlaan_2003',...
+                'baumgartner_2011',...
+                'pamlab_general',...
+                'pamlab_longcalls',...
+                'pamlab_clicks',...
+                'pamlab_hiresclicks',...
+                'pamlab_whistles'};
+            preset_name = validatestring(preset_name, valid_presets);
+
+            % initialize output
+            params = struct(...
+                'winfcn', [],...
+                'winsize', [],...
+                'novl', [],...
+                'nfft', []);
+
+            % process parameters
+            switch preset_name
+                case 'meridian'
+                    % parameters used by the MERIDIAN group to process
+                    % right whale upcalls, such as in Kirsebom et al.
+                    % (2020).
+                    params.winfcn = 'hamming'; % not actually sure what it is
+                    params.winsize = round(fs*0.256);
+                    params.novl = params.winsize - round(fs*0.032);
+                    params.nfft = 2^nextpow2(fs*(2048/8000));
+                case 'bio_narw_analysis'
+                    % parameters typically used at BIO to detect right
+                    % whale upcalls with LFDCS
+                    params.winfcn = 'hann';
+                    params.winsize = round(fs*(512/2000));
+                    params.novl = round(0.75*params.winsize);
+                    params.nfft = params.winsize;
+                case 'bio_muppet'
+                    % default parameters used in MUPPET for blue whale call
+                    % analysis
+                    params.winfcn = 'hamming';
+                    params.winsize = round(fs*(8192/8000));
+                    params.novl = round(params.winsize*0.9);
+                    params.nfft = params.winfcn;
+                case 'flogeras_aural'
+                    % parameters used by Dave Flogeras's "aural-features"
+                    % python package (MIGHT NEED REVISION). 
+                    params.winfcn = 'hamming'; % not actually sure what it is
+                    params.winsize = 256; % scipy default
+                    params.novl = round(0.5*params.winsize);
+                    params.nfft = 1024;
+                case 'vanderlaan_2003'
+                    % parameters used in Vanderlaan et al. (2003) to
+                    % characterize right whale calls in the Bay of Fundy.
+                    params.winfcn = 'hamming'; % not actually sure what it is
+                    params.winsize = round(fs*0.2);
+                    params.novl = params.winsize - round(fs*0.064);
+                    params.nfft = 2^nextpow2(fs/5);
+                case 'baumgartner_2011'
+                    % parameters used by Baumgartner and Mussoline (2011)
+                    % in their data analysis during their presentation of
+                    % LFDCS
+                    params.winfcn = 'hann';
+                    params.winsize = round(fs*(640/2048));
+                    params.novl = round(0.8*params.winsize);
+                    params.nfft = params.winsize;
+                case 'pamlab_general'
+                    % parameters from the "General (all call types)"
+                    % single-part setting in PAMlab
+                    params.winfcn = 'hamming';
+                    params.winsize = round(fs*0.128);
+                    params.novl = params.winsize - round(fs*0.032);
+                    params.nfft = 2^nextpow2(fs/2);
+                case 'pamlab_longcalls'
+                    % parameters from the "Long calls (blue whale)"
+                    % single-part setting in PAMlab
+                    params.winfcn = 'hamming';
+                    params.winsize = round(fs*2);
+                    params.novl = params.winsize - round(fs*0.5);
+                    params.nfft = 2^nextpow2(fs/0.4);
+                case 'pamlab_clicks'
+                    % parameters from the "Clicks" single-part setting in
+                    % PAMlab
+                    params.winfcn = 'hamming';
+                    params.winsize = round(fs*0.001);
+                    params.novl = params.winsize - round(fs*0.0005);
+                    params.nfft = 2^nextpow2(fs/128);
+                case 'pamlab_hiresclicks'
+                    % parameters from the "High-res clicks (beaked whale)"
+                    % single-part setting in PAMlab
+                    params.winfcn = 'hamming';
+                    params.winsize = round(fs*0.000266);
+                    params.novl = params.winsize - round(fs*0.00002);
+                    params.nfft = 2^nextpow2(fs/512);
+                case 'pamlab_whistles'
+                    % parameters from the "Whistles" single-part setting in
+                    % PAMlab
+                    params.winfcn = 'hamming';
+                    params.winsize = round(fs*0.05);
+                    params.novl = params.winsize - round(fs*0.01);
+                    params.nfft = 2^nextpow2(fs/4);
+                otherwise
+                    error('This preset lacks a definition, please update the code to add one!')
+            end
+        end
     end
 end
